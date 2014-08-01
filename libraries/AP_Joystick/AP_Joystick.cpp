b2736b911721e7ebc94e09e19f79bb9189cc5dec
Michael Day <mday299@yahoo.com>
2014-08-01 10:36:01 -0700

    Joystick: Added new library for lower-latency joystick.

---
+++ libraries/AP_Joystick/AP_Joystick.cpp
@@ -0,0 +1,61 @@
+#include "AP_Joystick.h"
+
+#include <AP_HAL.h>
+
+extern const AP_HAL::HAL& hal;
+
+const AP_Param::GroupInfo AP_Joystick::var_info[] PROGMEM = {
+    // @Param: ENABLED
+    // @Display Name: Joystick Enabled
+    // @Description: Boolean indicating whether joystick is active.
+    // @Units: Boolean
+    // @Range: 0 1
+    // @Increment: 1
+    // @User: Advanced
+    AP_GROUPINFO("ENABLED", 0, AP_Joystick, _enabled, 0),
+
+    // @Param: FS_TIME
+    // @Display Name: Joystick Failsafe Time
+    // @Description: If nonzero, specifics the amount of time in seconds after not receiving either a PWM value from the joystick or a hearbeat from the GCS before we engage the failsafe.
+    // @Units: Seconds 
+    // @Range: 0 255
+    // @Increment: 1
+    // @User: Advanced
+    AP_GROUPINFO("FS_TIME", 1, AP_Joystick, _failsafe_timeout, 2),
+
+    // @Param: FS_ACTION
+    // @Display Name: Joystick Failsafe Action
+    // @Description: Specifies action taken if telemetry link is lost during joystick operation
+    // @Values: 0:None,1:RTL,2:Kill_Throttle
+    // @User: Advanced
+    AP_GROUPINFO("FS_ACTION", 2, AP_Joystick, _failsafe_action, 1),
+
+    // @Param: LINK_DELAY
+    // @Display Name: MAVLink Delay
+    // @Description: Amount to delay MAVLink packets sending during joystick operation.  This prioritizes the link for joystick use.
+    // @Units: Seconds
+    // @Range: 0 5
+    // @Increment: 0.1
+    // @User: Advanced
+    AP_GROUPINFO("LINK_DELAY", 3, AP_Joystick, _mavlink_delay, 5.0f),
+
+    AP_GROUPEND
+};
+
+//constructor
+AP_Joystick::AP_Joystick() {
+    AP_Param::setup_object_defaults(this, var_info);
+}
+
+bool AP_Joystick::check_failsafe_ok() {
+    if (_enabled == 0 || _failsafe_action == NONE || _failsafe_timeout == 0) {
+        return true;
+    }
+
+    if (hal.scheduler->millis() - _last_heartbeat_ms > (_failsafe_timeout * 1000u)) {
+        return false;
+    }
+
+    return true;
+}
+
---
+++ libraries/AP_Joystick/AP_Joystick.h
@@ -0,0 +1,53 @@
+/*
+ * Maintains parameters for joystick operation.
+ * Also handles special failsafe for joystick operation: if the telemetry link
+ *      is lost then kill throttle.  Note the failsafe is optional
+ *
+ */
+#ifndef AP_Joystick_h
+#define AP_Joystick_h
+
+#include <AP_Common.h>
+#include <AP_Param.h>
+
+/// @class AP_Joystick
+/// @brief Maintains parameters for joystick / game pad operation
+class AP_Joystick {
+public:
+    typedef enum JS_FS_ACTION {
+        NONE = 0,
+        RTL,
+        KILL_THROTTLE
+    } JS_FS_ACTION;
+
+    AP_Joystick();
+
+    uint8_t get_enabled() { return _enabled; }
+    uint8_t get_failsafe_timeout() { return _failsafe_timeout; }
+    uint8_t get_failsafe_action() { return _failsafe_action; }
+    float get_mavlink_delay() { return _mavlink_delay; }
+
+    void set_last_heartbeat_ms(uint32_t last_hb_ms) { _last_heartbeat_ms = last_hb_ms; }
+
+    void set_enabled(uint8_t enable) { _enabled = enable; }
+
+    //return true if failsafe ok,
+    //false if failsafe should fire
+    bool check_failsafe_ok();
+
+    //parameter block
+    static const struct AP_Param::GroupInfo var_info[];
+
+protected:
+
+    uint32_t _last_heartbeat_ms;
+
+    //parameters
+    AP_Int8 _enabled;
+    AP_Int8 _failsafe_timeout;
+    AP_Int8 _failsafe_action;
+    AP_Float _mavlink_delay;
+    
+};
+
+#endif //AP_Joystick_h
