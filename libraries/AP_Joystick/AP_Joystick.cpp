ba41c0e5e6a810c8bb85b77122b31040ea9cf942
Matt Epperson <m.p.epperson@gmail.com>
2014-08-01 14:14:42 -0700

    Joystick: Deleted uneeded param

--- libraries/AP_Joystick/AP_Joystick.cpp
+++ libraries/AP_Joystick/AP_Joystick.cpp
@@ -5,15 +5,6 @@
 extern const AP_HAL::HAL& hal;
 
 const AP_Param::GroupInfo AP_Joystick::var_info[] PROGMEM = {
-    // @Param: ENABLED
-    // @Display Name: Joystick Enabled
-    // @Description: Boolean indicating whether joystick is active.
-    // @Units: Boolean
-    // @Range: 0 1
-    // @Increment: 1
-    // @User: Advanced
-    AP_GROUPINFO("ENABLED", 0, AP_Joystick, _enabled, 0),
-
     // @Param: FS_TIME
     // @Display Name: Joystick Failsafe Time
     // @Description: If nonzero, specifics the amount of time in seconds after not receiving either a PWM value from the joystick or a hearbeat from the GCS before we engage the failsafe.
@@ -21,14 +12,14 @@ const AP_Param::GroupInfo AP_Joystick::var_info[] PROGMEM = {
     // @Range: 0 255
     // @Increment: 1
     // @User: Advanced
-    AP_GROUPINFO("FS_TIME", 1, AP_Joystick, _failsafe_timeout, 2),
+    AP_GROUPINFO("FS_TIME", 0, AP_Joystick, _failsafe_timeout, 2),
 
     // @Param: FS_ACTION
     // @Display Name: Joystick Failsafe Action
     // @Description: Specifies action taken if telemetry link is lost during joystick operation
     // @Values: 0:None,1:RTL,2:Kill_Throttle
     // @User: Advanced
-    AP_GROUPINFO("FS_ACTION", 2, AP_Joystick, _failsafe_action, 1),
+    AP_GROUPINFO("FS_ACTION", 1, AP_Joystick, _failsafe_action, 1),
 
     // @Param: LINK_DELAY
     // @Display Name: MAVLink Delay
@@ -37,7 +28,7 @@ const AP_Param::GroupInfo AP_Joystick::var_info[] PROGMEM = {
     // @Range: 0 5
     // @Increment: 0.1
     // @User: Advanced
-    AP_GROUPINFO("LINK_DELAY", 3, AP_Joystick, _mavlink_delay, 5.0f),
+    AP_GROUPINFO("LINK_DELAY", 2, AP_Joystick, _mavlink_delay, 5.0f),
 
     AP_GROUPEND
 };
