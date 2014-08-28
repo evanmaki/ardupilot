1d8be12ca1ec3f75a29d0811e4eeef886a3863cc
Michael Day <mday299@yahoo.com>
2014-08-28 15:10:25 -0700

    AP_Joystick: saving work for the day.

--- libraries/AP_Joystick/AP_Joystick.h
+++ libraries/AP_Joystick/AP_Joystick.h
@@ -23,32 +23,20 @@ public:
     AP_Joystick();
 
     uint8_t get_enabled() { return _enabled; }
-    uint8_t get_failsafe_timeout() { return _failsafe_timeout; }
-    uint8_t get_failsafe_action() { return _failsafe_action; }
     uint8_t get_reset_stream_ticks() { return _reset_stream_ticks; }
     float get_mavlink_delay() { return _mavlink_delay; }
 
-    void set_last_heartbeat_ms(uint32_t last_hb_ms) { _last_heartbeat_ms = last_hb_ms; }
-
     void set_enabled(uint8_t enable) { _enabled = enable; }
     void set_reset_stream_ticks(uint8_t reset_stream_ticks) { _reset_stream_ticks = reset_stream_ticks; }
-
-    //return true if failsafe ok,
-    //false if failsafe should fire
-    bool check_failsafe_ok();
-
+   
     //parameter block
     static const struct AP_Param::GroupInfo var_info[];
 
 protected:
 
-    uint32_t _last_heartbeat_ms;
-
     //parameters
     AP_Int8 _enabled;
     AP_Int8 _reset_stream_ticks;
-    AP_Int8 _failsafe_timeout;
-    AP_Int8 _failsafe_action;
     AP_Float _mavlink_delay;
     
 };
